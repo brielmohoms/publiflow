3728b8175dcac45389bbf11dfdc8fddb39b73791
Noelvanelle <ntchindakuegouo@gmail.com>
2025-05-21 23:15:10 +0200

    add BibUploader and Layout

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,13 @@
 import './App.css';
+import Layout from './Layout';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout />
     </div>
   );
 }
 
 export default App;
+

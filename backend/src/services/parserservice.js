546c287738b238625a4e063eea74d88741c44780
brielmohoms <brielmohoms@yahoo.com>
2025-05-22 08:28:01 +0200

    update

--- backend/src/services/parserservice.js
+++ backend/src/services/parserservice.js
@@ -3,15 +3,18 @@ const bibtexParse = require('bibtex-parse');
 
 function convertBibtexToJson(inputFile, outputFile) {
     try {
+        
         if (!fs.existsSync(inputFile)) {
             throw new Error('Input file does not exist.');
         }
+
         const bibtexStr = fs.readFileSync(inputFile, 'utf8');
         const parsed = bibtexParse.entries(bibtexStr);
+
         fs.writeFileSync(outputFile, JSON.stringify(parsed, null, 2));
-        console.log('✅ JSON file created:', outputFile);
+        console.log('JSON file created:', outputFile);
     } catch (error) {
-        console.error('❌ Error:', error.message);
+        console.error('Error:', error.message);
     }
 }
 

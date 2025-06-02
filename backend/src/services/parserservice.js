24f320ba33eb4a2b9427df3beb8b0fba4c2c95fa
Nsangou Tatsinkou Tony Adams <tonytatsinkou@gmail.com>
2025-06-02 13:42:35 +0200

    all field entry can be edit

--- backend/src/services/parserservice.js
+++ backend/src/services/parserservice.js
@@ -1,21 +1,25 @@
 // Load the BibTeX parser library
 const bibtexParse = require('bibtex-parse-js');
 
+const bibtexFields = [
+  "address", "author", "booktitle", "chapter", "doi", "edition", "editor", "howpublished",
+  "institution", "isbn", "issn", "journal", "month", "note", "number", "organization",
+  "pages", "publisher", "school", "series", "title", "type", "url", "volume", "year",
+  "abstract", "annote", "crossref", "key", "language", "location", "pdf", "keywords"
+];
+
 module.exports = {
-  
   parseBib: async (bibText) => {
-    
-    // Use bibtex-parse-js to convert the raw text into a JS array
     const rawEntries = bibtexParse.toJSON(bibText);
 
-    // Map each parsed entry to your desired shape
-    return rawEntries.map((item, index) => ({
-      id: index + 1,                         
-      type: item.entryType || '',           
-      key: item.citationKey || '',           
-      title: item.entryTags.title || '',    
-      author: item.entryTags.author || '',   
-      year: item.entryTags.year || '',    
-    }));
+    return rawEntries.map((item, index) => {
+      const entry = { id: index + 1 };
+      bibtexFields.forEach(field => {
+        entry[field] = item.entryTags && item.entryTags[field] ? item.entryTags[field] : '';
+      });
+      entry.type = item.entryType || '';
+      entry.key = item.citationKey || '';
+      return entry;
+    });
   }
 };
